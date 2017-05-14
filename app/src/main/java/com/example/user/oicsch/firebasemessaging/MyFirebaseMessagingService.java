cd3c254d55a2d6bed53e4bb846b8eedbe72aeb43
Akash shrestha <aakashshrestha31@gmail.com>
2017-05-15 01:06:38 +0545

    Firemessaging commit

--- app/src/main/java/com/example/user/oicsch/firebasemessaging/MyFirebaseMessagingService.java
+++ app/src/main/java/com/example/user/oicsch/firebasemessaging/MyFirebaseMessagingService.java
@@ -14,28 +14,37 @@
  * limitations under the License.
  */
 
-package com.example.user.oicsch;
+package com.example.user.oicsch.firebasemessaging;
 
 import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.media.RingtoneManager;
 import android.net.Uri;
 import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
+import com.example.user.oicsch.R;
+import com.example.user.oicsch.notification;
 import com.firebase.jobdispatcher.FirebaseJobDispatcher;
 import com.firebase.jobdispatcher.GooglePlayDriver;
 import com.firebase.jobdispatcher.Job;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 public class MyFirebaseMessagingService extends FirebaseMessagingService {
 
-    private static final String TAG = "MyFirebaseMsgService";
+    private static final String TAG = "poo";
+    private Bitmap bitmap;
 
     /**
      * Called when message is received.
@@ -61,8 +70,11 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
 
         // Check if message contains a data payload.
         if (remoteMessage.getData().size() > 0) {
-            Log.d(TAG, "Message data payload: " + remoteMessage.getData());
-
+            String title=remoteMessage.getData().get("title");
+            String message=remoteMessage.getData().get("message");
+            String image_url=remoteMessage.getData().get("image_url");
+            bitmap= getBitmapfromUrl(image_url);
+            sendNotification(title,message,bitmap);
             if (/* Check if data needs to be processed by long running job */ true) {
                 // For long-running tasks (10 seconds or more) use Firebase Job Dispatcher.
                 scheduleJob();
@@ -76,7 +88,7 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
         // Check if message contains a notification payload.
         if (remoteMessage.getNotification() != null) {
             Log.d(TAG, "Message Notification Body: " + remoteMessage.getNotification().getBody());
-            sendNotification(remoteMessage.getNotification().getBody());
+
         }
 
         // Also if you intend on generating your own notifications as a result of a received FCM
@@ -107,10 +119,11 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
 
     /**
      * Create and show a simple notification containing the received FCM message.
-     *
-     * @param messageBody FCM message body received.
+     *  @param title
+     * @param message
+     * @param image FCM message body received.
      */
-    private void sendNotification(String messageBody) {
+    private void sendNotification(String title, String message, Bitmap image) {
         Intent intent = new Intent(getApplicationContext(), notification.class);
         intent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
         PendingIntent pendingIntent = PendingIntent.getActivity(getApplicationContext(), 0 /* Request code */, intent,0);
@@ -118,9 +131,11 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
         Uri defaultSoundUri= RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
         NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
                 .setSmallIcon(R.mipmap.ic_launcher)
-                .setContentTitle("OICSch")
-                .setContentText(messageBody)
+                .setContentTitle(title)
+                .setContentText(message)
                 .setAutoCancel(true)
+                .setStyle(new NotificationCompat.BigPictureStyle()
+                        .bigPicture(image))
                 .setVibrate(new long[] { 1000, 1000, 1000, 1000, 1000 })
                 .setLights(0xff00ff00,200,200)
                 .setSound(defaultSoundUri)
@@ -128,5 +143,22 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
                 .setContentIntent(pendingIntent);
         NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
         notificationManager.notify(0 /* ID of notification */, notificationBuilder.build());
+    }
+    public Bitmap getBitmapfromUrl(String imageUrl) {
+        try {
+            URL url = new URL(imageUrl);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setDoInput(true);
+            connection.connect();
+            InputStream input = connection.getInputStream();
+            Bitmap bitmap = BitmapFactory.decodeStream(input);
+            return bitmap;
+
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+            return null;
+
+        }
     }
 }
